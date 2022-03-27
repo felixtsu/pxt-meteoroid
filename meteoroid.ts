4de5d879f95a618614f5a4da618e8bc33f428f2c
felixtsu <humaeks@gmail.com>
2022-03-27 14:40:04 +0800

    更新 meteoroid.ts

--- meteoroid.ts
+++ meteoroid.ts
@@ -6,19 +6,23 @@ namespace meteoroid {
 
     const METEOROID_DESTINATION_X = "METEOROID_DESTINATION_X"
     const METEOROID_DESTINATION_Y = "METEOROID_DESTINATION_Y"
+    const METEOROID_OFFSET = "METEOROID_OFFSET"
 
     //%block
     //%blockid=meteoroid_launch_meteoriod %block.loc.zh-CN="将 %sprite=variables_get(mySprite) 以速度 %velocity 在 %delayMillis 毫秒后砸向地面"
-    export function launchMeteoroid(sprite:Sprite, toLocation:tiles.Location, velocity:number, delayMillis:number) {
-        launchMeteoroidToPosition(sprite, toLocation.x, toLocation.y, velocity, delayMillis)
+    export function launchMeteoroid(sprite:Sprite, toLocation:tiles.Location, velocity:number, delayMillis:number, offset:number=0) {
+        launchMeteoroidToPosition(sprite, toLocation.x, toLocation.y, velocity, delayMillis, offset)
     }
 
     //%block
-    //%blockid=meteoroid_launch_meteoriod %block.loc.zh-CN="将 %sprite=variables_get(mySprite) 以速度 %velocity 在 %delayMillis 毫秒后砸向地面"
-    export function launchMeteoroidToPosition(sprite: Sprite, x:number, y:number, velocity: number, delayMillis: number) {
+    //%blockid=meteoroid_launch_meteoriod %block.loc.zh-CN="将 %sprite=variables_get(mySprite) 以速度 %velocity 在 %delayMillis 毫秒后砸向地面 || 距离地面高度 %offset 生效"
+    export function launchMeteoroidToPosition(sprite: Sprite, x:number, y:number, velocity: number, delayMillis: number, offset:number=0) {
         sprite.setFlag(SpriteFlag.Ghost, true)
 
+        sprites.setDataNumber(sprite, METEOROID_OFFSET, offset)
+
         let shade = shader.createImageShaderSprite(sprite.image, shader.ShadeLevel.One)
+        shade.setFlag(SpriteFlag.GhostThroughWalls,true)
         spriteShades[sprite.id] = shade
 
         shade.setPosition(x, y)
@@ -28,24 +32,27 @@ namespace meteoroid {
             spriteShades[sprite.id] = null
         })
 
-        let timeoutHolder = setTimeout(() => {
-            let delta = 10000
-
+        let intervalHolder = setInterval(() => {
             let detinationX = sprites.readDataNumber(shade, METEOROID_DESTINATION_X)
             let detinationY = sprites.readDataNumber(shade, METEOROID_DESTINATION_Y)
-            
+
+            let delta = 0
             if (detinationX && detinationY) {
                 // 有横向移动的陨石
                 // 判断阴影是否移动到特定的位置
                 delta = Math.sqrt(Math.pow(y - detinationY, 2) + Math.pow(x - detinationX, 2))
-            } else {
-                // 固定位置的陨石
-                // 判断陨石的坠落位置
-                delta = Math.sqrt(Math.pow(y - sprite.y, 2) + Math.pow(x - sprite.x, 2))
-   
-            }
+                
+            } 
             if (delta < 1.5) {
-                shade.destroy()
+                console.log(`${y}, ${sprite.y}, ${x}, ${sprite.x},`)
+                let deltaHeight = Math.sqrt(Math.pow(y - sprite.y, 2) + Math.pow(x - sprite.x, 2))
+                console.log(`${deltaHeight}, ${offset}`)
+                if (deltaHeight < offset) {
+                    sprite.setFlag(SpriteFlag.Ghost, false)
+                }
+                if (deltaHeight < 1.5) {
+                    shade.destroy()
+                }
             }
         }, 25)
 
@@ -54,7 +61,7 @@ namespace meteoroid {
             sprite.setPosition(shade.x, shade.y)
             sprite.vx = 0
             sprite.vy = 0
-            clearTimeout(timeoutHolder)
+            clearInterval(intervalHolder)
         })
 
         sprite.setPosition(x, y)
@@ -73,8 +80,13 @@ namespace meteoroid {
         sprite.vx = deltaX / delayMillis * 1000
     }
 
-    export function redirectTo(sprite: Sprite, x: number, y: number, delayMillis:number) {
-        changeVelocity(sprite, x, y, delayMillis)
+    export function redirectTo(sprite: Sprite, x: number, y: number, delayMillis:number) {        
+        let offset = sprites.readDataNumber(sprite, METEOROID_OFFSET)
+        if (!offset) {
+            offset = 0
+        }
+
+        changeVelocity(sprite, x, y - offset, delayMillis)
 
         let shade = spriteShades[sprite.id]
         changeVelocity(shade, x, y, delayMillis)
