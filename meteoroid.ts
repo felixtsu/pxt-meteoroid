0b83c06f0e0697357ddae3ed410159060332162a
felixtsu <humaeks@gmail.com>
2022-03-27 08:30:18 +0800

    更新 pxt.json, meteoroid.ts, test.ts

--- meteoroid.ts
+++ meteoroid.ts
@@ -4,6 +4,11 @@
 namespace meteoroid {
 
 
+    let spriteShades :SparseArray<Sprite> = {}
+
+    const METEOROID_DESTINATION_X = "METEOROID_DESTINATION_X"
+    const METEOROID_DESTINATION_Y = "METEOROID_DESTINATION_Y"
+
     //%block
     //%blockid=meteoroid_launch_meteoriod %block.loc.zh-CN="将 %sprite=variables_get(mySprite) 以速度 %velocity 在 %delayMillis 毫秒后砸向地面"
     export function launchMeteoroid(sprite:Sprite, toLocation:tiles.Location, velocity:number, delayMillis:number) {
@@ -16,32 +21,71 @@ namespace meteoroid {
         sprite.setFlag(SpriteFlag.Ghost, true)
 
         let shade = shader.createImageShaderSprite(sprite.image, shader.ShadeLevel.One)
+        spriteShades[sprite.id] = shade
+
         shade.setPosition(x, y)
 
         sprite.onDestroyed(() => {
             shade.destroy()
+            spriteShades[sprite.id] = null
         })
 
         let timeoutHolder = setTimeout(() => {
-            let delta = y - sprite.y
+            let delta = 10000
+
+            let detinationX = sprites.readDataNumber(shade, METEOROID_DESTINATION_X)
+            let detinationY = sprites.readDataNumber(shade, METEOROID_DESTINATION_Y)
+            
+            if (detinationX && detinationY) {
+                // 有横向移动的陨石
+                // 判断阴影是否移动到特定的位置
+                delta = Math.sqrt(Math.pow(y - detinationY, 2) + Math.pow(x - detinationX, 2))
+            } else {
+                // 固定位置的陨石
+                // 判断陨石的坠落位置
+                delta = Math.sqrt(Math.pow(y - sprite.y, 2) + Math.pow(x - sprite.x, 2))
+   
+            }
             if (delta < 1.5) {
                 shade.destroy()
             }
-        }, 100)
+        }, 25)
 
         shade.onDestroyed(() => {
             sprite.setFlag(SpriteFlag.Ghost, false)
             sprite.setPosition(shade.x, shade.y)
+            sprite.vx = 0
             sprite.vy = 0
             clearTimeout(timeoutHolder)
         })
 
-
-
         sprite.setPosition(x, y)
-        sprite.y -= velocity * delayMillis - sprite.height / 2
+        sprite.y -= velocity * delayMillis / 1000 - sprite.height / 2
         sprite.vy = velocity
 
-        shade.lifespan = delayMillis * 1000
+        shade.lifespan = delayMillis
+    }
+
+
+    function changeVelocity(sprite:Sprite, x:number, y:number, delayMillis:number) {
+        let deltaY = y - sprite.y
+        sprite.vy = deltaY / delayMillis * 1000
+        
+        let deltaX = x - sprite.x
+        sprite.vx = deltaX / delayMillis * 1000
+    }
+
+    export function redirectTo(sprite: Sprite, x: number, y: number, delayMillis:number) {
+        changeVelocity(sprite, x, y, delayMillis)
+
+        let shade = spriteShades[sprite.id]
+        changeVelocity(shade, x, y, delayMillis)
+        
+        // 横向移动陨石，设置目标位置
+        sprites.setDataNumber(shade, METEOROID_DESTINATION_X, x)
+        sprites.setDataNumber(shade, METEOROID_DESTINATION_Y, y)
+
+        // 重设lifespan
+        shade.lifespan = delayMillis
     }
 }
--- test.ts
+++ test.ts
@@ -1,1 +1,58 @@
 // 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.sayText("砸中我了", 1000)
+})
+let meteoroidSprite: Sprite = null
+// 在此处添加您的代码
+scene.setBackgroundColor(14)
+let hero = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(hero)
+for (let index = 0; index < 5; index++) {
+    meteoroidSprite = sprites.create(img`
+        . . . . . . . c c c a c . . . . 
+        . . c c b b b a c a a a c . . . 
+        . c c a b a c b a a a b c c . . 
+        . c a b c f f f b a b b b a . . 
+        . c a c f f f 8 a b b b b b a . 
+        . c a 8 f f 8 c a b b b b b a . 
+        c c c a c c c c a b c f a b c c 
+        c c a a a c c c a c f f c b b a 
+        c c a b 6 a c c a f f c c b b a 
+        c a b c 8 6 c c a a a b b c b c 
+        c a c f f a c c a f a c c c b . 
+        c a 8 f c c b a f f c b c c c . 
+        . c b c c c c b f c a b b a c . 
+        . . a b b b b b b b b b b b c . 
+        . . . c c c c b b b b b c c . . 
+        . . . . . . . . c b b c . . . . 
+        `, SpriteKind.Enemy)
+    meteoroid.launchMeteoroidToPosition(
+        meteoroidSprite,
+        randint(60, 100),
+        randint(40, 80),
+        50,
+        randint(2000, 3000)
+    )
+}
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, ()=> {
+    meteoroid.redirectTo(meteoroidSprite, randint(60, 100), randint(40, 80), randint(2000, 3000))
+})
